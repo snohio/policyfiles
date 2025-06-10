3f8565486bf958a3b8812669eb116884e98e2953
Mike Butler <snohio@gmail.com>
2025-06-10 12:31:40 -0400

    Add dotnet cookbook to win_diiv for win_choco changes

--- base_win_diiv.rb
+++ base_win_diiv.rb
@@ -12,14 +12,17 @@ default_source :supermarket do |s|
 end
 
 # run_list: chef-client will run these recipes in the order specified.
-run_list 'chef-client::default', 'diiv::default', 'win_choco::default'
+run_list 'chef-client::default', 'diiv::default', 'dotnet::default', 'win_choco::default'
 
 # Specify a custom source for a single cookbook:
 # cookbook 'example_cookbook', path: '../cookbooks/example_cookbook'
 cookbook 'diiv', git: 'https://github.com/snohio/diiv.git', branch: 'main'
 cookbook 'win_choco', git: 'https://github.com/snohio/win_choco.git', branch: 'main'
 cookbook 'chef-client', git: 'https://github.com/snohio/chef-client.git', branch: 'main'
+cookbook 'dotnet', git: 'https://github.com/snohio/dotnet.git', branch: 'main'
 
 default['audit']['reporter'] = 'chef-server-automate', 'cli'
 default['sa']['mode'] = 'client'
 default['software']['vscode'] = true
+default['dotnet']['framework'] = true
+default['dotnet']['version']['required'] = '4.8.03761'
