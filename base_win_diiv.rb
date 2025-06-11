6932bbdb6a8b9d5c0822f350ca2c771fd43c7dc1
Mike Butler <snohio@gmail.com>
2025-06-11 07:38:23 -0400

    base_win_diiv cookbook order update

--- base_win_diiv.rb
+++ base_win_diiv.rb
@@ -12,7 +12,7 @@ default_source :supermarket do |s|
 end
 
 # run_list: chef-client will run these recipes in the order specified.
-run_list 'chef-client::default', 'diiv::default', 'dotnet::default', 'win_choco::default'
+run_list 'chef-client::default', 'dotnet::default', 'win_choco::default', 'diiv::default'
 
 # Specify a custom source for a single cookbook:
 # cookbook 'example_cookbook', path: '../cookbooks/example_cookbook'
