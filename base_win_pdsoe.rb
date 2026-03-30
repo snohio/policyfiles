a1db9c8640ca3c4256955fc446fc6c7ba2d639d1
Mike Butler <michael.butler@progress.com>
2026-03-30 13:55:23 -0400

    add dotnet to pdsoe cookbook for win_choco changes

--- base_win_pdsoe.rb
+++ base_win_pdsoe.rb
@@ -8,13 +8,16 @@ name 'base_win_pdsoe'
 default_source :supermarket
 
 # run_list: chef-client will run these recipes in the order specified.
-run_list 'chef-client::default', 'win_choco::default', 'pdsoe::default'
+run_list 'chef-client::default', 'dotnet::default', 'win_choco::default', 'pdsoe::default'
 
 # Specify a custom source for a single cookbook:
 # cookbook 'example_cookbook', path: '../cookbooks/example_cookbook'
 cookbook 'chef-client', git: 'https://github.com/snohio/chef-client.git', branch: 'main'
+cookbook 'dotnet', git: 'https://github.com/snohio/dotnet.git', branch: 'main'
 cookbook 'win_choco', git: 'https://github.com/snohio/win_choco.git', branch: 'main'
 cookbook 'pdsoe', git: 'https://github.com/snohio/pdsoe.git', branch: 'main'
 
 default['audit']['reporter'] = 'chef-server-automate', 'cli'
-
+default['dotnet']['framework'] = true
+default['dotnet']['version']['required'] = '4.8.03761' # Server
+# default['dotnet']['version']['required'] = '4.8.04084' # Desktop
